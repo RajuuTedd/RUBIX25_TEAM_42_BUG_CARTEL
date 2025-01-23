a1820bd9cdce776efc488b61e81f92b3b77cf0e0
Shikhar-13 <shikharguptaoct13@gmail.com>
2025-01-23 20:54:59 +0530

    donor dashboard + login + signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Dashboard from './pages/dashboard'
+import LoginPage from './pages/login'
+import SignupForm from './pages/signup'
+import Dash_donor from './pages/dash_donor'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
     <Router>
             <Routes>
                 <Route path="/" element={<Dashboard />} />
+                <Route path="/donor" element={<Dash_donor />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/Signup" element={<SignupForm />} />
                 
                 {/* Other routes */}
             </Routes>
